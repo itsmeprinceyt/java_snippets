ae25e5be018179394b4abf667c5b6052fa6eb231
itsmeprinceyt <114930668+itsmeprinceyt@users.noreply.github.com>
2024-08-22 18:43:22 +0530

    upload

---
+++ firstgui.java
@@ -0,0 +1,12 @@
+package com.firstjavaproject;
+import javax.swing.JOptionPane;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String name = JOptionPane.showInputDialog("Whats your name little bro: ");
+		JOptionPane.showMessageDialog(null,"Oil up! "+name);
+
+	}
+
+}
